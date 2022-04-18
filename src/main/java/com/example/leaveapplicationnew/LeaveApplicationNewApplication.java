4764e665c1e33ad3fa3c7314e7aafe059b07f646
echoece <echoece@gmail.com>
2022-04-19 02:18:09 +0600

    Initial commit

---
+++ src/main/java/com/example/leaveapplicationnew/LeaveApplicationNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.leaveapplicationnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveApplicationNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeaveApplicationNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/leaveapplicationnew/LeaveApplicationNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leaveapplicationnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveApplicationNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
